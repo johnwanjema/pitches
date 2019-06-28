b5821359c814944f8f78a54c529cb37a03094ed6
johnwanjema <jonwanjema@gmail.com>
2019-06-28 12:48:03 +0300

    added index navbar and base an rendered them

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,9 @@ from .. import db
 
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = "pitches"
+    return render_template('index.html',title = title)
