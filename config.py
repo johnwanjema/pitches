cc2c94bb60abc3fa5c5af45096cb5dffe343664e
johnwanjema <jonwanjema@gmail.com>
2019-06-29 13:28:18 +0300

    added sectret key

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://john:1234@localhost/ip'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
     pass
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+export SECRET_KEY='1234'
 python3.6 manage.py server
