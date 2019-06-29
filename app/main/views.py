33fa7ef1ebe7ba2f9cb0c83b440695aa59f7ae7d
johnwanjema <jonwanjema@gmail.com>
2019-06-29 16:19:40 +0300

    added function for new pitches and rendered it

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,11 @@
 from flask import render_template, request, redirect, url_for,abort
 from . import main
 from .. import db
-from .forms import RegistrationForm,LoginForm
+from .forms import PitchForm
 from flask import render_template, flash,request
 from flask import render_template,redirect,url_for
-from ..models import User
-from .forms import RegistrationForm
+from ..models import User,Pitch
+
 from .. import db
 from flask_login import login_user,logout_user,login_required
 
@@ -19,5 +19,25 @@ def index():
     '''
     title = "pitches"
     return render_template('index.html',title = title)
+
+@main.route('/movie/<int:pitch_id>')
+def pitch(pitch_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    form = PitchForm()
+    if form.validate_on_submit():
+           
+
+        # Updated pitch instance
+        new_pitch = Pitch(pitch = form.pitch.data,name = form.name.data)
+
+        #   save pitch method
+        # new_pitch.save_pitch()
+        # return redirect(url_for('.',id = movie.id ))
+
+    title = "pitches"
+    return render_template('pitch.html',id = pitch_id,pitch_form = form)
 
 
