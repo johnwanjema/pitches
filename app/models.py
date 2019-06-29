8f0326b8b98eef8dd28be4c895a8fc90c9d1022d
johnwanjema <jonwanjema@gmail.com>
2019-06-29 23:08:21 +0300

    updated models

--- app/models.py
+++ app/models.py
@@ -9,37 +9,15 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class Comment(db.Model):
-    __tablename__ = 'comment'
-    id = db.Column(db.Integer,primary_key = True)
-    comment = db.column(db.String)
-    posted = db.Column(db.DateTime,default=datetime.utcnow)
-    users = db.relationship('User',backref = 'comment',lazy="dynamic")
-    def save_review(self):
-        db.session.add(self)
-        db.session.commit()
-    
-    @classmethod
-    def get_reviews(cls,id):
-        comment = Comment.query.filter_by(comment_id=id).all()
-        return Comment
-    
-    def __repr__(self):
-      return f'User {self.comment}'
-
-
-
-
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True,index = True)    
+    email = db.Column(db.String(255),unique = True,index = True)
+    pitch = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
     password_hash = db.Column(db.String(255))
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
-    comment_id = db.Column(db.Integer,db.ForeignKey('comment.id'))
-    
+
     @property
     def password(self):
         raise AttributeError('You cannnot read the password attribute')
@@ -52,32 +30,38 @@ class User(UserMixin,db.Model):
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
 
-    def __repr__(self):
-        return f'User {self.username}'
 
-   
-    def __repr__(self):
-        return f'User {self.username}'
-
-
-class Pitch(db.Model):
-    __tablename__ = 'pitch'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    pitch  = db.column(db.String(255))
-    upvote = db.column(db.Integer)
-    downvote = db.column(db.Integer)
-    users = db.relationship('User',backref = 'pitch',lazy="dynamic")
-
-    def save_pitch(self):
+    def save_user(self):
         db.session.add(self)
         db.session.commit()
 
-    @classmethod
-    def get_pitch(cls,id):
-        reviews = Pitch.query.filter_by(user_id=id).all()
-        return pitch
-
     def __repr__(self):
-        return f'Pitch {self.name}'
+        return f'User {self.username}'
+
+
+
+class Pitch(db.Model):
+
+    __tablename__ = 'pitch'
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_category = db.Column(db.String)
+    pitch_title = db.Column(db.String)
+    pitch_upvotes = db.Column(db.Integer)
+    pitch_downvotes = db.Column(db.Integer)
+    posted = db.Column(db.Time,default=datetime.utcnow())
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+
+
+    def save_review(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+
+    @classmethod
+    def get_reviews(cls,id):
+
+        pitches = Pitch.query.filter_by(movie_id=id).all()
+        return pitches
+
