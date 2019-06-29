6f8f0e33ad21d307e1f75bace3fa88e347b4f832
johnwanjema <jonwanjema@gmail.com>
2019-06-29 12:47:47 +0300

    added classes

--- app/models.py
+++ app/models.py
@@ -1,31 +1,29 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from . import login_manager
+
 from datetime import datetime
 
 
-class Comments(db.Model):
-
-    __tablename__ = 'comments'
-
+class Comment(db.Model):
+    __tablename__ = 'comment'
     id = db.Column(db.Integer,primary_key = True)
-    comment = db.column(db.string)    
+    comment = db.column(db.String)
     posted = db.Column(db.DateTime,default=datetime.utcnow)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-
-
+    users = db.relationship('User',backref = 'comment',lazy="dynamic")
     def save_review(self):
         db.session.add(self)
         db.session.commit()
-
+    
     @classmethod
     def get_reviews(cls,id):
-        comment = Comments.query.filter_by(comment_id=id).all()
-        return Comments
-
+        comment = Comment.query.filter_by(comment_id=id).all()
+        return Comment
+    
     def __repr__(self):
-        return f'User {self.comment}'
+      return f'User {self.comment}'
+
+
 
 
 class User(UserMixin,db.Model):
@@ -34,21 +32,11 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)    
-    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
     password_hash = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
+    comment_id = db.Column(db.Integer,db.ForeignKey('comment.id'))
 
-    @property
-    def password(self):
-        raise AttributeError('You cannnot read the password attribute')
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-
-    def verify_password(self,password):
-        return check_password_hash(self.password_hash,password)
-
+   
     def __repr__(self):
         return f'User {self.username}'
 
@@ -58,10 +46,10 @@ class Pitch(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(255))
-    title  = db.column(db.string(255))
+    pitch  = db.column(db.String(255))
     upvote = db.column(db.Integer)
     downvote = db.column(db.Integer)
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    users = db.relationship('User',backref = 'pitch',lazy="dynamic")
 
     def __repr__(self):
         return f'Pitch {self.name}'
